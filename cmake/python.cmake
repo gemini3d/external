b40ae46321f8d88cfb96ffded1fb9a71678a70f2
Michael Hirsch <scivision@users.noreply.github.com>
2022-03-03 13:48:54 -0500

    detail feedback for Windows Python

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -4,19 +4,19 @@ string(JSON python_sha256 GET ${json_meta} python sha256)
 if(WIN32)
   # https://pythondev.readthedocs.io/windows.html
 
-  message(FATAL_ERROR "On Windows, Python is available from Microsoft Store.")
-  # requires Visual Studio, which doesn't work with other external libraries
+  message(WARNING "On Windows, Python is available from Microsoft Store.
+Python building on Windows requires Visual Studio, which doesn't work with other external libraries.")
 
-  # ExternalProject_Add(python
-  # URL ${python_url}
-  # URL_HASH SHA256=${python_sha256}
-  # CONFIGURE_COMMAND ""
-  # BUILD_COMMAND <SOURCE_DIR>/PCBuild/build.bat -p x64
-  # INSTALL_COMMAND ""
-  # TEST_COMMAND ""
-  # CONFIGURE_HANDLED_BY_BUILD ON
-  # INACTIVITY_TIMEOUT 15
-  # )
+  ExternalProject_Add(python
+  URL ${python_url}
+  URL_HASH SHA256=${python_sha256}
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND <SOURCE_DIR>/PCBuild/build.bat
+  INSTALL_COMMAND ""
+  TEST_COMMAND ""
+  CONFIGURE_HANDLED_BY_BUILD ON
+  INACTIVITY_TIMEOUT 15
+  )
 
 else()
   # Linux prereqs: https://devguide.python.org/setup/#linux
