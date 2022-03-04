4ad44e4ce9923dfe3e7fc572f0842878480597ca
Michael Hirsch <scivision@users.noreply.github.com>
2022-03-03 20:13:21 -0500

    python:ssl option

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -31,35 +31,8 @@ else()
     message(FATAL_ERROR "Python requires GNU Make.")
   endif()
 
-  string(JSON ffi_url GET ${json_meta} ffi url)
-  string(JSON ffi_sha256 GET ${json_meta} ffi sha256)
-
-  set(ffi_args
-  --prefix=${CMAKE_INSTALL_PREFIX}
-  CC=${CMAKE_C_COMPILER}
-  CXX=${CMAKE_CXX_COMPILER}
-  )
-
-  ExternalProject_Add(ffi
-  URL ${ffi_url}
-  URL_HASH SHA256=${ffi_sha256}
-  CONFIGURE_COMMAND <SOURCE_DIR>/configure ${ffi_args}
-  BUILD_COMMAND ${MAKE_EXECUTABLE} -j
-  INSTALL_COMMAND ${MAKE_EXECUTABLE} -j install
-  TEST_COMMAND ""
-  CONFIGURE_HANDLED_BY_BUILD ON
-  INACTIVITY_TIMEOUT 15
-  )
-
-  ExternalProject_Add_Step(ffi
-  autogen
-  COMMAND <SOURCE_DIR>/autogen.sh
-  DEPENDEES download
-  DEPENDERS configure
-  WORKING_DIRECTORY <SOURCE_DIR>
-  )
-  # autogen.sh needs to be executed in SOURCE_DIR, not in build directory
-
+  include(cmake/ffi.cmake)
+  include(cmake/ssl.cmake)
 
   ExternalProject_Add(python
   URL ${python_url}
@@ -70,7 +43,7 @@ else()
   TEST_COMMAND ""
   CONFIGURE_HANDLED_BY_BUILD ON
   INACTIVITY_TIMEOUT 15
-  DEPENDS ffi
+  DEPENDS "ffi;ssl"
   )
 
 endif()
---
+++ cmake/ssl.cmake
@@ -0,0 +1,28 @@
+# installs OpenSSL CMAKE_INSTALL_PREFIX/lib64/lib{crypto,ssl}.{a,so}
+
+find_package(OpenSSL)
+if(OpenSSL_FOUND)
+  add_custom_target(ssl)
+  return()
+endif()
+
+string(JSON ssl_url GET ${json_meta} ssl git)
+string(JSON ssl_tag GET ${json_meta} ssl tag)
+
+set(ssl_args
+--prefix=${CMAKE_INSTALL_PREFIX}
+--openssldir=${CMAKE_INSTALL_PREFIX}
+CC=${CMAKE_C_COMPILER}
+)
+
+ExternalProject_Add(ssl
+GIT_REPOSITORY ${ssl_url}
+GIT_TAG ${ssl_tag}
+GIT_SHALLOW true
+CONFIGURE_COMMAND <SOURCE_DIR>/config ${ssl_args}
+BUILD_COMMAND ${MAKE_EXECUTABLE} -j
+INSTALL_COMMAND ${MAKE_EXECUTABLE} -j install
+TEST_COMMAND ""
+CONFIGURE_HANDLED_BY_BUILD ON
+INACTIVITY_TIMEOUT 15
+)
