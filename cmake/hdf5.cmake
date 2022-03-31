6f1f8b9f1e2b180f75de77afd76ea4a6eb172927
Michael Hirsch <scivision@users.noreply.github.com>
2022-03-31 16:27:12 -0400

    hdf5 mpi_root

--- cmake/hdf5.cmake
+++ cmake/hdf5.cmake
@@ -14,8 +14,8 @@ set(hdf5_cmake_args
 -DHDF5_BUILD_TOOLS:BOOL=$<NOT:$<BOOL:${hdf5_parallel}>>
 -DHDF5_ENABLE_PARALLEL:BOOL=$<BOOL:${hdf5_parallel}>
 )
-if(hdf5_parallel AND MPI_ROOT)
-  list(APPEND hdf5_cmake_args -DMPI_ROOT:PATH=${MPI_ROOT})
+if(hdf5_parallel)
+  list(APPEND hdf5_cmake_args -DMPI_ROOT:PATH=${CMAKE_INSTALL_PREFIX})
 endif()
 
 set(hdf5_deps zlib)
