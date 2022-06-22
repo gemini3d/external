fe95e4d48d35683b138b316f75a2a37f7c6784cc
Michael Hirsch <scivision@users.noreply.github.com>
2022-06-22 17:36:35 -0400

    cray: actual toolchain

--- cray.cmake
+++ cray.cmake
@@ -1,61 +1,89 @@
 # loads modules on Cray system
 # canNOT use from Project CMakeLists.txt
 # it's OK to run again if you're not sure if it was already run.
+#
+# NOTE: your Cray system may have different versions/paths, treat this like a template.
+#
+# NOTE: to specify install directory, run like:
+#   cmake -DCMAKE_INSTALL_PREFIX=<install_dir> -P cray.cmake
 
-cmake_minimum_required(VERSION 3.20...3.23)
+cmake_minimum_required(VERSION 3.20)
 
-if(NOT CMAKE_INSTALL_PREFIX)
-  message(FATAL_ERROR "Please specify libraries install directory:
-  cmake -DCMAKE_INSTALL_PREFIX=<install_dir> -P ${CMAKE_CURRENT_LIST_FILE}")
-endif()
+# --- user options
 
 option(intel "use intel compiler instead of default GCC")
 
-# the module commands only affect the current process, not the parent shell
+# --- module names (may be different on your system)
+
+set(gcc_mod gcc)
+set(pecray PrgEnv-cray)
+set(pegnu PrgEnv-gnu)
+set(peintel PrgEnv-intel)
+
+# --- main script
+
+# propagate options
 if(intel)
-  cmake_path(SET BINARY_DIR ${CMAKE_CURRENT_LIST_DIR}/build-intel)
+  set(ENV{_toolintel} ${intel})
 else()
-  cmake_path(SET BINARY_DIR ${CMAKE_CURRENT_LIST_DIR}/build)
+  set(intel "$ENV{_toolintel}")
 endif()
 
 find_package(EnvModules REQUIRED)
 
-env_module(load cpe/22.03 OUTPUT_VARIABLE out RESULT_VARIABLE ret)
-if(ret)
-  message(STATUS "load cpe/22.03 error ${ret}: ${out}")
+function(gcc_toolchain)
+
+set(CXXFLAGS $ENV{CXXFLAGS})
+if(CXXFLAGS MATCHES "--gcc-toolchain")
+  return()
 endif()
+
+env_module(load ${gcc_mod} OUTPUT_VARIABLE out RESULT_VARIABLE ret)
+message(STATUS "load ${gcc_mod}:    ${out}  ${ret}")
+
+find_program(cc NAMES gcc REQUIRED)
+
+execute_process(COMMAND ${cc} -dumpversion
+OUTPUT_VARIABLE gcc_vers
+COMMAND_ERROR_IS_FATAL ANY
+)
+if(gcc_vers VERSION_LESS 9.1)
+  message(FATAL_ERROR "GCC toolchain >= 9.1 is required for oneAPI")
+endif()
+
+execute_process(COMMAND ${cc} -v
+OUTPUT_VARIABLE gcc_verb
+ERROR_VARIABLE gcc_verb
+COMMAND_ERROR_IS_FATAL ANY
+)
+
+set(pat "--prefix=([/a-zA-Z0-9_\\-\\.]+)")
+string(REGEX MATCH "${pat}" gcc_prefix "${gcc_verb}")
+
+if(CMAKE_MATCH_1)
+  string(APPEND CXXFLAGS " --gcc-toolchain=${CMAKE_MATCH_1}")
+  set(ENV{CXXFLAGS} ${CXXFLAGS})
+else()
+  message(WARNING "GCC toolchain not found")
+endif()
+
+endfunction(gcc_toolchain)
+
+# the module commands only affect the current process, not the parent shell
+env_module_list(mods)
 
 if(intel)
-  env_module(load intel-oneapi/2022.0.2 OUTPUT_VARIABLE out RESULT_VARIABLE ret)
-  env_module_swap(PrgEnv-cray PrgEnv-intel OUTPUT_VARIABLE out RESULT_VARIABLE ret)
+  cmake_path(SET BINARY_DIR ${CMAKE_CURRENT_LIST_DIR}/build-intel)
+
+  if(NOT mods MATCHES "${peintel}")
+    message(FATAL_ERROR "Cray Intel programming environment ${peintel} isn't loaded.")
+  endif()
+  # need new enough GCC toolchain
+  gcc_toolchain()
 else()
-  env_module_swap(PrgEnv-cray PrgEnv-gnu OUTPUT_VARIABLE out RESULT_VARIABLE ret)
-endif()
-if(ret)
-  message(STATUS "swap PrgEnv error ${ret}: ${out}")
-endif()
+  cmake_path(SET BINARY_DIR ${CMAKE_CURRENT_LIST_DIR}/build)
 
-env_module(load python OUTPUT_VARIABLE out RESULT_VARIABLE ret)
-
-# too compiler specific
-# env_module(load cray-hdf5 OUTPUT_VARIABLE out RESULT_VARIABLE ret)
-# if(ret)
-#   message(STATUS "load cray-hdf5 error ${ret}: ${out}")
-# endif()
-
-if(NOT intel)
-  env_module(load cray-mpich OUTPUT_VARIABLE out RESULT_VARIABLE ret)
-  if(ret)
-    message(STATUS "load cray-mpich error ${ret}: ${out}")
+  if(NOT mods MATCHES "${pegnu}")
+    message(FATAL_ERROR "Cray GCC programming environment ${pegnu} isn't loaded.")
   endif()
 endif()
-
-execute_process(
-COMMAND ${CMAKE_COMMAND} -S${CMAKE_CURRENT_LIST_DIR} -B${BINARY_DIR} -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
-COMMAND_ERROR_IS_FATAL ANY
-)
-
-execute_process(
-COMMAND ${CMAKE_COMMAND} --build ${BINARY_DIR}
-COMMAND_ERROR_IS_FATAL ANY
-)
