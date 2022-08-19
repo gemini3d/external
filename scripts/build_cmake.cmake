3dbcc446f3432d59ed40d718825912cc811143c4
Michael Hirsch <scivision@users.noreply.github.com>
2022-08-19 17:31:19 -0400

    install_cmake: need to regex plain version file to avoid junk getting in.
    add print to help diagnose user system

--- scripts/build_cmake.cmake
+++ scripts/build_cmake.cmake
@@ -2,13 +2,17 @@ cmake_minimum_required(VERSION 3.13)
 
 set(bindir ${CMAKE_CURRENT_LIST_DIR}/../build/cmake_build)
 
-set(config_args
--Dversion=${version}
+set(args)
+if(version)
+  list(APPEND args -Dversion=${version})
+endif()
+if(prefix)
+  list(APPEND args -DCMAKE_INSTALL_PREFIX:PATH=${prefix})
+endif()
+
+execute_process(COMMAND ${CMAKE_COMMAND} ${args}
 -B${bindir}
 -S${CMAKE_CURRENT_LIST_DIR}/build_cmake
-)
-
-execute_process(COMMAND ${CMAKE_COMMAND} ${config_args}
 RESULT_VARIABLE ret
 )
 
