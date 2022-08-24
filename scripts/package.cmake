da5c9bea7813a7956ee9273c55577eaef2e3522c
Michael Hirsch <scivision@users.noreply.github.com>
2022-08-24 15:23:25 -0400

    package: also archive offline_install.cmake

--- scripts/package.cmake
+++ scripts/package.cmake
@@ -198,6 +198,12 @@ file(APPEND ${manifest_txt}
 
 endforeach()
 
+# append scripts/offline_install.cmake
+file(APPEND ${manifest_txt}
+"offline_install.cmake
+")
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/offline_install.cmake DESTINATION ${outdir}/)
+
 
 # --- create one big archive file of all the archive files above
 
