b79a6bafb915ee5673031f32628b86aa40b40c0c
Michael Hirsch <scivision@users.noreply.github.com>
2022-08-29 09:19:59 -0400

    functionalize

--- cmake/ExtProj.cmake
+++ cmake/ExtProj.cmake
@@ -1,34 +1,6 @@
 include(ExternalProject)
 
-if(CMAKE_VERSION VERSION_LESS 3.19)
-  include(${CMAKE_CURRENT_LIST_DIR}/Modules/JsonParse.cmake)
-endif()
-
-
-function(get_tag name)
-
-if(CMAKE_VERSION VERSION_LESS 3.19)
-  set(tag ${meta.${name}.tag})
-else()
-  string(JSON tag GET ${json_meta} ${name} tag)
-endif()
-
-set(tag ${tag} PARENT_SCOPE)
-
-endfunction(get_tag)
-
-
-function(get_hash name)
-
-if(CMAKE_VERSION VERSION_LESS 3.19)
-  set(sha256 ${meta.${name}.sha256})
-else()
-  string(JSON sha256 GET ${json_meta} ${name} sha256)
-endif()
-
-set(sha256 ${sha256} PARENT_SCOPE)
-
-endfunction(get_hash)
+include(${CMAKE_CURRENT_LIST_DIR}/GetJson.cmake)
 
 
 function(extproj name url_type cmake_args depends)
@@ -65,17 +37,14 @@ if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.18)
   list(APPEND extproj_args GIT_REMOTE_UPDATE_STRATEGY "CHECKOUT")
 endif()
 
-if(CMAKE_VERSION VERSION_LESS 3.19)
-  sbeParseJson(meta json_meta)
-  set(url ${meta.${name}.url})
-else()
-  string(JSON url GET ${json_meta} ${name} url)
+if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.19)
   list(APPEND extproj_args
   INACTIVITY_TIMEOUT 60
   CONFIGURE_HANDLED_BY_BUILD true
   )
 endif()
 
+get_url(${name} ${json_meta})
 
 if(local)
   # archive file on this computer or network drive
@@ -99,7 +68,7 @@ if(local)
 
 elseif(url_type STREQUAL "git")
 
-  get_tag(${name})
+  get_tag(${name} ${json_meta})
 
   ExternalProject_Add(${name}
   GIT_REPOSITORY ${url}
@@ -109,7 +78,7 @@ elseif(url_type STREQUAL "git")
   )
 elseif(url_type STREQUAL "archive")
 
-  get_hash(${name})
+  get_hash(${name} ${json_meta})
 
   ExternalProject_Add(${name}
   URL ${url}
@@ -151,62 +120,3 @@ DEPENDEES build
 endif()
 
 endfunction(extproj)
-
-
-function(fetch_source name url_type)
-
-set(extproj_args
-CMAKE_ARGS ${cmake_args}
-TLS_VERIFY true
-UPDATE_DISCONNECTED true
-TEST_COMMAND ""
-)
-if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.18)
-  list(APPEND extproj_args GIT_REMOTE_UPDATE_STRATEGY "CHECKOUT")
-endif()
-
-if(CMAKE_VERSION VERSION_LESS 3.19)
-  sbeParseJson(meta json_meta)
-  set(url ${meta.${name}.url})
-else()
-  string(JSON url GET ${json_meta} ${name} url)
-  list(APPEND extproj_args
-  INACTIVITY_TIMEOUT 60
-  CONFIGURE_HANDLED_BY_BUILD true
-  )
-endif()
-
-if(url_type STREQUAL "git")
-
-get_tag(${name})
-
-ExternalProject_Add(${name}
-GIT_REPOSITORY ${url}
-GIT_TAG ${tag}
-GIT_SHALLOW true
-GIT_PROGRESS true
-${extproj_args}
-CONFIGURE_COMMAND ""
-BUILD_COMMAND ${CMAKE_COMMAND} -Dpkg=${name} -Darchive=${PROJECT_BINARY_DIR}/package/${name}.tar.bz2 -Ddir:PATH=<SOURCE_DIR> -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/tar.cmake
-INSTALL_COMMAND ""
-)
-
-elseif(url_type STREQUAL "archive")
-
-get_hash(${name})
-
-ExternalProject_Add(${name}
-URL ${url}
-URL_HASH SHA256=${sha256}
-${extproj_args}
-CONFIGURE_COMMAND ""
-BUILD_COMMAND ${CMAKE_COMMAND} -E copy <DOWNLOADED_FILE> ${PROJECT_BINARY_DIR}/package/
-INSTALL_COMMAND ""
-DOWNLOAD_NO_EXTRACT true
-DOWNLOAD_NAME ${name}.bz2
-)
-
-endif()
-
-
-endfunction(fetch_source)
