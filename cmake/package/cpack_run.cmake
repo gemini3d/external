3aa5470e267f93a4995129ce1fe6c6feeee13083
Michael Hirsch <scivision@users.noreply.github.com>
2022-09-21 13:07:19 -0400

    cpack pre_build doesn't work when fed to cpack command line

--- cmake/package/cpack_run.cmake
+++ cmake/package/cpack_run.cmake
@@ -14,12 +14,12 @@ COMMAND ${CMAKE_CPACK_COMMAND}
 -G TBZ2
 --config ${cfg}
 -B ${bindir}
--DCPACK_PRE_BUILD_SCRIPTS=${CMAKE_CURRENT_LIST_DIR}/cpack_hdf5.cmake
 RESULT_VARIABLE ret
 ERROR_VARIABLE err
 )
 # "-B ${bindir}" in case the sub-project has an arbitrary CPACK_PACKAGE_DIRECTORY specified in itself
 # -DCPACK_PACKAGE_INSTALL_DIRECTORY="" didn't help for HDF5 with arbitrary source-package-only subdir HDF_Group/HDF5/${HDF5_VERSION}
+# -DCPACK_PRE_BUILD_SCRIPTS=${CMAKE_CURRENT_LIST_DIR}/cpack_hdf5.cmake didn't get triggered--many such options don't work when fed into CPack command line
 
 if(NOT ret EQUAL 0)
   message(FATAL_ERROR "${name}: failed to package using ${cfg}: ${ret}:
