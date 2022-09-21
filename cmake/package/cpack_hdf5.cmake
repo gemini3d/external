95b0f417b8f52681065b6a0d4cb76564c21eb590
Michael Hirsch <scivision@users.noreply.github.com>
2022-09-21 12:48:48 -0400

    cpack:hdf5: shell cmd needed

--- cmake/package/cpack_hdf5.cmake
+++ cmake/package/cpack_hdf5.cmake
@@ -1,3 +1,5 @@
+set(CMAKE_EXECUTE_PROCESS_COMMAND_ECHO STDOUT)
+
 file(READ ${CMAKE_CURRENT_LIST_DIR}/../libraries.json meta)
 string(JSON HDF5_VERSION GET ${meta} "hdf5" "version")
 
@@ -14,4 +16,17 @@ endif()
 
 message(STATUS "cpack_hdf5: HDF5 CPack source layout found, moving ${hdf5_top} => ${CMAKE_INSTALL_PREFIX}")
 
-file(RENAME ${hdf5_top} ${CMAKE_INSTALL_PREFIX})
+# fails because "directory not empty"
+#file(RENAME ${hdf5_top}/* ${CMAKE_INSTALL_PREFIX}/)
+#execute_process(COMMAND ${CMAKE_COMMAND} -E rename ${hdf5_top} ${CMAKE_INSTALL_PREFIX})
+
+if(UNIX)
+  execute_process(COMMAND sh -c "mv ${hdf5_top}/* ${CMAKE_INSTALL_PREFIX}/"
+  RESULT_VARIABLE ret
+  )
+elseif(WIN32)
+  message(FATAL_ERROR "TODO: implement Windows version of cpack_hdf5")
+endif()
+if(NOT ret EQUAL "0")
+  message(FATAL_ERROR "cpack_hdf5: failed to move HDF5 CPack source layout")
+endif()
