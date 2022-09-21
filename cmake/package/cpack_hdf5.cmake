8f5e02be3265ba79002c5db1add8b82a5e679e6d
Michael Hirsch <scivision@users.noreply.github.com>
2022-09-21 12:58:43 -0400

    cpack: ensure pre_build script runs

--- cmake/package/cpack_hdf5.cmake
+++ cmake/package/cpack_hdf5.cmake
@@ -10,7 +10,7 @@ NO_DEFAULT_PATH
 )
 
 if(NOT hdf5_top)
-  message(WARNING "cpack_hdf5: HDF5 CPack source layout not as expected, skipping patch.")
+  message(DEBUG "cpack_hdf5: HDF5 CPack source layout not detected, returning.")
   return()
 endif()
 
@@ -20,12 +20,14 @@ message(STATUS "cpack_hdf5: HDF5 CPack source layout found, moving ${hdf5_top} =
 #file(RENAME ${hdf5_top}/* ${CMAKE_INSTALL_PREFIX}/)
 #execute_process(COMMAND ${CMAKE_COMMAND} -E rename ${hdf5_top} ${CMAKE_INSTALL_PREFIX})
 
-if(UNIX)
+if(WIN32)
+  execute_process(COMMAND pwsh -c "Move-Item -Path ${hdf5_top} -Destination ${CMAKE_INSTALL_PREFIX}"
+  RESULT_VARIABLE ret
+  )
+else()
   execute_process(COMMAND sh -c "mv ${hdf5_top}/* ${CMAKE_INSTALL_PREFIX}/"
   RESULT_VARIABLE ret
   )
-elseif(WIN32)
-  message(FATAL_ERROR "TODO: implement Windows version of cpack_hdf5")
 endif()
 if(NOT ret EQUAL "0")
   message(FATAL_ERROR "cpack_hdf5: failed to move HDF5 CPack source layout")
