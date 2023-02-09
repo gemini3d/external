3b2e1cede9b1e0a5b2424990393658152d82adbf
scivision <scivision@users.noreply.github.com>
2023-02-09 12:02:27 -0500

    build_mpich: sensible defaults

--- scripts/build_mpich.cmake
+++ scripts/build_mpich.cmake
@@ -1,29 +1,34 @@
 # USAGE:
 # cmake -Dprefix=~/mpich -P build_mpich.cmake
-
-
 cmake_minimum_required(VERSION 3.19)
 
 if(APPLE)
   find_program(gcc NAMES gcc-14 gcc-13 gcc-12 gcc-11 REQUIRED)
 endif()
 
-set(bindir ${CMAKE_CURRENT_LIST_DIR}/../build/mpich_build)
+set(args -Dmpich:BOOL=true)
 
-set(args -Dmpich:BOOL=true -DCMAKE_C_COMPILER:FILEPATH=${gcc})
+if(gcc)
+  list(APPEND args -DCMAKE_C_COMPILER:FILEPATH=${gcc})
+endif()
 
 if(prefix)
   list(APPEND args -DCMAKE_INSTALL_PREFIX:PATH=${prefix})
 endif()
 
-execute_process(COMMAND ${CMAKE_COMMAND} ${args}
--B${bindir}
--S${CMAKE_CURRENT_LIST_DIR}/mpi
+if(NOT bindir)
+  set(bindir ${CMAKE_CURRENT_LIST_DIR}/../build/mpich_build)
+endif()
+
+execute_process(COMMAND ${CMAKE_COMMAND}
+  ${args}
+  -B${bindir}
+  -S${CMAKE_CURRENT_LIST_DIR}/mpi
 RESULT_VARIABLE ret
 )
 
 if(ret EQUAL 0)
-  message(STATUS "MPICH build")
+  message(STATUS "MPICH build in ${bindir}")
 else()
   message(FATAL_ERROR "MPICH failed to configure.")
 endif()
